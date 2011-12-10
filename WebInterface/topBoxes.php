4980043edf47ef7d45d5eb00d587b44801830aa1
Odd Stråbø <oddstr13@openshell.no>
2011-12-10 01:26:54 +0100

    started cleaning code; currently working on reindentation etc. found several instances of missing <body> start tag, and seemingly in the air </p> tags (no starting <p> tag) much copypaste of code?

--- WebInterface/topBoxes.php
+++ WebInterface/topBoxes.php
@@ -1,29 +1,49 @@
 <div id="profile-box">
-		<table cellspacing="3px">
-		<tr>
-		<td>
-		<img width="64px" src="http://minotar.net/avatar/<?php echo $user ?>" />
-		</td>
-		<td>
+  <table cellspacing="3px">
+    <tr>
+      <td>
+        <img width="64px" src="http://minotar.net/avatar/<?php echo $user ?>" />
+      </td>
+      <td>
         <p>Name: &nbsp;&nbsp;<?php echo $user?><?php if ($isAdmin == "true"){ echo " ADMIN"; } ?><br/>
-		<?php if ($useMySQLiConomy){ ?>
-		Money: &nbsp;<?php echo $currencyPrefix.$iConRow['2'].$currencyPostfix?><br />
-		<?php }else{ ?>
-        Money: &nbsp;$<?php echo $playerRow['3']?><br />
-		<?php } ?>
-        Mail: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $mailCount ?><br />
+<?php
+// TODO: printf();
+// TODO: get rid of table for formating, use css instead
+	if ($useMySQLiConomy) {
+?>
+Money: &nbsp;
+<?php
+		echo $currencyPrefix.$iConRow['2'].$currencyPostfix;
+?>
+<br />
+<?php
+	} else {
+?>
+Money: &nbsp;$
+<?php
+	echo $playerRow['3'];
+?>
+<br />
+<?php
+	}
+?>
+Mail: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<?php
+	echo $mailCount;
+?>
+<br />
         </p>
-		</td>
-		</tr>
-		</table>
-        
+      </td>
+    </tr>
+  </table>
 </div>
-      <div id="link-box">
-        <p><a href="index.php">Home</a><br />
-        <a href="myitems.php">My Items</a><br />
-        <a href="myauctions.php">My Auctions</a><br />
-		<a href="info.php">Item Info</a><br />
-		<a href="transactionLog.php">Transaction Log</a><br />
-        <a href="logout.php">Logout</a></p>
-        
-        </div>
+<div id="link-box">
+  <p>
+    <a href="index.php">Home</a><br />
+    <a href="myitems.php">My Items</a><br />
+    <a href="myauctions.php">My Auctions</a><br />
+    <a href="info.php">Item Info</a><br />
+    <a href="transactionLog.php">Transaction Log</a><br />
+    <a href="logout.php">Logout</a>
+  </p>
+</div>
